021472e9e69e566769f3d3e31d83d76f52887d0e
naba93980 <72063612+naba93980@users.noreply.github.com>
2024-01-13 22:02:30 +0530

    publisher, subscriber, subscription

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -2,15 +2,39 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 
+import reactor.core.scheduler.Schedulers;
+
 public class Exercise2 {
 
+    
+    /** 
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
-
+    	
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().log()
 
+                // side effect of Subscriber.OnSubcribe  in callbback
+                .doOnSubscribe((e) -> System.out.println("subscribed on thread ------" + Thread.currentThread().getName()))
+                .doOnRequest((e) -> System.out.println(e+" request made on thread ------" + Thread.currentThread().getName()))
+                
+                // side effect of Subscriber.onNext(data) in callback
+                .doOnNext(data -> System.out.println(data + " emmited on thread "+ Thread.currentThread().getName()))
+                .map((e)->{System.out.println(e+" mappiung on thread "+ Thread.currentThread().getName()); return e+100;})
+                .subscribe(
+                		// like hookOnNext()
+                        (e) -> System.out.println(e + "printed on thread " + Thread.currentThread().getName()),
+                        
+                        // like hookOnError()
+                        (err) -> System.out.println(err),
+                        
+                        // like hookOnComplete()
+                        () -> System.out.println("complete on thread ---------- " + Thread.currentThread().getName()));
+                            
         // Print all users in the ReactiveSources.userFlux stream
         // TODO: Write code here
 
--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -2,8 +2,16 @@ package io.javabrains.reactiveworkshop;
 
 import java.io.IOException;
 
+import org.reactivestreams.Subscription;
+
+import reactor.core.publisher.BaseSubscriber;
+
 public class Exercise5 {
 
+    /** 
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
 
         // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
@@ -12,10 +20,26 @@ public class Exercise5 {
         // TODO: Write code here
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().log().subscribe(new MySubscriber<>());
 
         System.out.println("Press a key to end");
         System.in.read();
     }
+}
 
+class MySubscriber<T> extends BaseSubscriber<T> {
+
+    @Override
+    protected void hookOnNext(Object value) {
+    	System.out.println(value);
+        System.out.println(value + " " +Thread.currentThread().getName());
+        request(1);
+    }
+
+    @Override
+    protected void hookOnSubscribe(Subscription subscription) {
+        System.out.println("subscribed on " + Thread.currentThread().getName());
+        subscription.request(5);
+    }
+    
 }
